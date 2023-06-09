cfeeef087e9d7b5511202f10f4095ddae29a4d96
Emmanuel Andrade <emmanuel.andrade@gmail.com>
2023-06-09 13:24:43 -0300

    finalização da tela com dados cadastrais

--- projetos/tela_login/lib/pages/dados_cadastrais.dart
+++ projetos/tela_login/lib/pages/dados_cadastrais.dart
@@ -25,6 +25,22 @@ class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
   var linguagens =[];
   var linguagensRepository = LinguagensRepository();
   var linguagensSelecionadas = [];
+  double salarioEscolhido = 0;
+  var tempoExperiencia =0;
+
+
+  List<DropdownMenuItem<int>> returnItens(int quantidadeMaxima){
+    var itens = <DropdownMenuItem<int>>[];
+    for (var i=0; i<quantidadeMaxima; i++){
+      itens.add(DropdownMenuItem(
+        value: i,
+        child: Text(i.toString()),
+      ));
+    }
+    return itens;
+  }
+
+  bool salvando = false;
 
   @override
   void initState() {
@@ -39,7 +55,9 @@ class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
         appBar: AppBar(title: Text(widget.texto)),
         body: Padding(
           padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-          child: ListView( //necessário usar esse widget para não ter problema com invasão de área do teclado.
+          child: salvando 
+          ? const Center(child: CircularProgressIndicator()) 
+          : ListView( //necessário usar esse widget para não ter problema com invasão de área do teclado.
             children: [
               const TextLabel(
                   texto:
@@ -102,10 +120,96 @@ class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
                     }
                 )).toList(),
               ],),
+              const TextLabel(texto: "Tempo de Experiência"),
+              DropdownButton(
+                value:tempoExperiencia,
+                isExpanded: true,
+                items:returnItens(50),
+                onChanged: (value){
+                  setState(() {
+                    tempoExperiencia = int.parse(value.toString());
+                  });
+
+              }),
+              
+              TextLabel(texto: "Pretensão Salarial: R\$ ${salarioEscolhido.round().toString()} "),
+              Slider(
+                min:0.0,
+                max: 15000,
+                value: salarioEscolhido,
+                onChanged: (double value){
+                  setState(() {
+                    salarioEscolhido = value;
+                    // debugPrint(salarioEscolhido.toString());
+                  });
+
+              }),
               TextButton(
                   onPressed: () {
+                    setState(() {
+                      salvando = false;
+                    });
+
+                    if(nomeController.text.trim().length <= 3){
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Nome inválido"),
+                      ));
+                      return;
+                    }
+                    if(dataNascimento==null){
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Data de Nascimento Inválida!"),
+                      ));
+                      return;
+                    }
+                    if(nivelSelecionado.trim()==""){
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Selecione um nível de experiência!"),
+                      ));
+                      return;
+                    }
+                    if(linguagensSelecionadas.isEmpty){
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Selecione pelo menos 1 linguagem!"),
+                      ));
+                      return;
+                    }
+                    if(salarioEscolhido==0){
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Pretensão salarial incorreta!"),
+                      ));
+                      return;
+                    }
+                    if(tempoExperiencia==0){
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Tempo de experiência precisa ser maior que 0!"),
+                      ));
+                      return;
+                    }
+                    
+                    setState(() {
+                      
+                      salvando = true;
+                    });
+
+                    Future.delayed(const Duration(seconds: 2), () {
+                      setState(() {
+                      salvando = false;
+                      });
+                      Navigator.pop(context);
+                    });
+
+
                     debugPrint(nomeController.text);
                     debugPrint(dataNascimento.toString());
+                    debugPrint(nivelSelecionado.toString());
+                    debugPrint(linguagensSelecionadas.toString());
+                    debugPrint(salarioEscolhido.toString());
+                    debugPrint(tempoExperiencia.toString());
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Dados salvos com sucesso!"),
+                        backgroundColor: Colors.green,
+                      ));
                   },
                   child: const Text("Salvar")),
             ],
