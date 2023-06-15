70ce3c8f9f1a71f69756b934be675945057fc459
Emmanuel Andrade <emmanuel.andrade@gmail.com>
2023-06-14 22:29:03 -0300

    inicializaçãodo repo do projeto de app do IMC

---
+++ projetos/imc_calculator/android/app/src/main/kotlin/comp/app_login/eabs/imc_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package comp.app_login.eabs.imc_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ projetos/imc_calculator/lib/pages/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  PageController controller = PageController(initialPage: 0);
+  int posicaoPagina = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+    ));
+  }
+}
